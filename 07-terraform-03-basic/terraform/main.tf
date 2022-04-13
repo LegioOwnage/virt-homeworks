3e1c66743f4fdc704dfa7b31899b87140b1bf9da
legioownage <legionftw@yandex.ru>
2022-04-13 19:12:21 +0300

    homework
    Signed-off-by: legioownage <legionftw@yandex.ru>

--- 07-terraform-03-basic/terraform/main.tf
+++ 07-terraform-03-basic/terraform/main.tf
@@ -34,17 +34,61 @@ locals {
   }
 }
 
+//Пример count
+
+#locals {
+#  vm_instance_count_map = {
+#    stage = 1
+#    prod  = 2
+#  }
+#}
+#
+#
+#resource "yandex_compute_instance" "vm" {
+#  name = local.yandex_instance_name_map[terraform.workspace]
+#
+#  count = local.vm_instance_count_map[terraform.workspace]
+#
+#  resources {
+#    cores  = 2
+#    memory = 2
+#  }
+#
+#  boot_disk {
+#    initialize_params {
+#      image_id = "fd86cpunl4kkspv0u25a"
+#    }
+#  }
+#
+#  network_interface {
+#    subnet_id = "${yandex_vpc_subnet.default.id}"
+#    nat       = true
+#  }
+#
+#  metadata = {
+#    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+#  }
+#}
+
+//Пример for_each
+
 locals {
-  vm_instance_count_map = {
-    stage = 1
-    prod  = 2
+  instances = {
+    "standard-v1" = local.yandex_instance_name_map[terraform.workspace]
   }
 }
 
-resource "yandex_compute_instance" "vm" {
-  name = local.yandex_instance_name_map[terraform.workspace]
+resource "yandex_compute_instance" "vm_for_each" {
 
-  count = local.vm_instance_count_map[terraform.workspace]
+  for_each = local.instances
+
+  name = each.value
+
+  platform_id = each.key
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   resources {
     cores  = 2
@@ -67,6 +111,7 @@ resource "yandex_compute_instance" "vm" {
   }
 }
 
+
   resource "yandex_vpc_network" "default" {
     name = "net"
   }
@@ -77,109 +122,3 @@ resource "yandex_compute_instance" "vm" {
     network_id     = "${yandex_vpc_network.default.id}"
     v4_cidr_blocks = ["192.168.10.0/24"]
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## for each example
-#
-#variable "hostnames" {
-#    default = {
-#        "prod" = {
-#            "name" = "prod",
-#            "platform_id" = "standard-v1",
-#            #"os" = "fd87va5cc00gaq2f5qfb",
-#            "zone" = "ru-central1-a"
-#            #"folder_id" = var.yandex_folder_id
-#        },
-#        "stage" = {
-#            "name" = "stage",
-#            "platform_id" = "standard-v1",
-#            #"os" = "fd87va5cc00gaq2f5qfb",
-#            "zone" = "ru-central1-a",
-#            #"folder_id" = var.yandex_folder_id
-#        }
-#    }
-#}
-#
-#resource "yandex_compute_instance" "default" {
-#    for_each = var.hostnames
-#    name         = each.value.name
-#    zone         = each.value.zone
-#
-#    boot_disk {
-#        initialize_params {
-#            image_id = "fd87uq4tagjupcnm376a"
-#        }
-#    }
-#
-#    network_interface {
-#    subnet_id = "yandex_vpc_subnet.default.id"
-#    nat       = true
-#    }
-#
-#  resources {
-#    cores  = 2
-#    memory = 2
-#  }
-#    metadata = {
-#    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-#  }
-#}
-#
-#  resource "yandex_vpc_network" "default" {
-#    name = "net"
-#  }
-#
-#  resource "yandex_vpc_subnet" "default" {
-#    name           = "subnet"
-#    zone           = "ru-central1-a"
-#    network_id     = "yandex_vpc_network.default.id"
-#    v4_cidr_blocks = ["10.128.0.0/24"]
-#  }
-#
-#
-#
-#resource "yandex_compute_instance" "vm" {
-#  for_each = {
-#    "prod"  = { vm_size = "blabla", zone = "ru-central1-a" }
-#    "stage" = { vm_size = "blabla", zone = "ru-central1-a" }
-#  }
-#  name     = each.key
-#  resources {
-#    cores  = 2
-#    memory = 2
-#  }
-#  boot_disk {
-#    initialize_params {
-#      image_id = "fd87uq4tagjupcnm376a"
-#    }
-#  }
-#
-#  network_interface {
-#    subnet_id = "yandex_vpc_subnet.default.id"
-#    nat       = true
-#  }
-#
-#  #  resources "yandex_vpc_subnet" "default" {
-#  #    name           = "subnet"
-#  #    zone           = "ru-central1-a"
-#  #    network_id     = "yandex_vpc_network.default.id"
-#  #    v4_cidr_blocks = ["10.128.0.0/24"]
-#  #    metadata = {
-#  #    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-#  #  }
-#}
